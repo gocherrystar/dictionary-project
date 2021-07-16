096a10c44e9b3c8392b88302265be2d2f46d143a
gocherrystar <79139792+gocherrystar@users.noreply.github.com>
2021-07-16 13:29:49 +1000

    Added dictionary api intergration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         
        </header>
+       <main>
+         <Dictionary />
+       </main>
+       <footer className="App-footer"> This code was created by Yue Ajioka and is{" "}
+    <a href="https://github.com/gocherrystar/dictionary-project" target="blank" rel="noopener noreferrer" >open-sourced on GitHub</a>
+    </footer>
+       </div>
     </div>
   );
 }
