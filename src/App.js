a3b8b79501d7df865f012597dfb6563d2b585cbf
gocherrystar <79139792+gocherrystar@users.noreply.github.com>
2021-07-25 13:19:39 +1000

    Add App name and styling

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <div className="container">
       <header className="App-header">
+        <h1>Dictionary</h1>
        </header>
         
        <main>
